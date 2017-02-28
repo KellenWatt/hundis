0c4576d4c5ad61b05c396ae85c7b98badc606edd
jniemeyer46 <jjniemeyer46@gmail.com>
2017-02-28 14:51:49 -0600

    Updating the homepage

--- hundis/config/routes.rb
+++ hundis/config/routes.rb
@@ -10,4 +10,7 @@ Rails.application.routes.draw do
   get 'static_pages/User_Account'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'static_pages#Home'
+  
 end
