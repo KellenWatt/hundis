0a86295729478cad7ad6c8eb9ee4d770244e7819
Samuel K. Grush <Samuel@Grush.org>
2017-03-18 18:57:06 -0500

    Updated routes, moved page header into a partial, updated Login page for OAuth

--- hundis/config/routes.rb
+++ hundis/config/routes.rb
@@ -4,36 +4,43 @@ Rails.application.routes.draw do
 
   get '/Home',          to: redirect('/')
 
-  get  '/login',        to: 'static_pages#login'
-  get  '/register',     to: 'static_pages#register'
-  post '/login',        to: 'auth#login'
-  post '/register',     to: 'auth#register'
+  # Authorization
+  get   '/auth/login',    to: 'static_pages#login'
+  post  '/auth/login',    to: 'sessions#create'
+  get   '/auth/register', to: 'static_pages#register'
+  post  '/auth/register', to: 'users#create'
+  get   '/auth/:provider/callback',  to: 'sessions#create'
+  get   '/auth/failure',  to: redirect('/auth/login')
+  get   '/auth/logout',   to: 'sessions#destroy', as: 'logout'
+
+  resources :sessions, only: [:create, :destroy]
+
 
   # Subsection Homepages
-  get '/problems',    to: 'static_pages#problems'
-  get '/users',       to: 'static_pages#users'
-  get '/tournaments', to: 'static_pages#tournaments'
+  get   '/problems',    to: 'static_pages#problems'
+  get   '/users',       to: 'static_pages#users'
+  get   '/tournaments', to: 'static_pages#tournaments'
 
   # Problems Subsection
-  get '/problems/statistics', to: 'problems#stats'
-  get '/problems/:id',        to: 'problems#show', constraints: { id: id_cnstrt }
-  get '/problems/:name',      to: 'problems#show'
-  get '/problems/:id/submit/',        to: 'problems#showUpload',    constraints: { id: id_cnstrt }
-  post '/problems/:id/submit/code',   to: 'problems#uploadCode',    constraints: { id: id_cnstrt }
-  post '/problems/:id/submit/output', to: 'problems#uploadOutput',  constraints: { id: id_cnstrt }
-  
+  get   '/problems/statistics', to: 'problems#stats'
+  get   '/problems/:id',        to: 'problems#show', constraints: { id: id_cnstrt }
+  get   '/problems/:name',      to: 'problems#show'
+  get   '/problems/:id/submit/',        to: 'problems#showUpload',    constraints: { id: id_cnstrt }
+  post  '/problems/:id/submit/code',    to: 'problems#uploadCode',    constraints: { id: id_cnstrt }
+  post  '/problems/:id/submit/output',  to: 'problems#uploadOutput',  constraints: { id: id_cnstrt }
+
   # Users Subsection
-  get '/users/current',             to: 'users#show'
-  get '/users/current/statistics',  to: 'users#stats'
-  get '/users/:id',                 to: 'users#show',   constraints: { id: id_cnstrt }
-  get '/users/:id/statistics',      to: 'users#stats',  constraints: { id: id_cnstrt }
-  
+  get   '/users/current',             to: 'users#show'
+  get   '/users/current/statistics',  to: 'users#stats'
+  get   '/users/:id',                 to: 'users#show',   constraints: { id: id_cnstrt }
+  get   '/users/:id/statistics',      to: 'users#stats',  constraints: { id: id_cnstrt }
+
   # Tourtnaments Subsection
-  get '/tournaments/:id',             to: 'tournaments#show', constraints: { id: id_cnstrt }
-  get '/tournaments/:id/statistics',  to: 'tournaments#stats', constraints: { id: id_cnstrt }
+  get   '/tournaments/:id',             to: 'tournaments#show', constraints: { id: id_cnstrt }
+  get   '/tournaments/:id/statistics',  to: 'tournaments#stats', constraints: { id: id_cnstrt }
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root 'static_pages#Home'
-  
+  root  'static_pages#Home'
+
 end
