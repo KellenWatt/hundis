ecc84cdb742602df618ce775c2ac98ec1b8c630a
Samuel K. Grush <Samuel@Grush.org>
2017-03-19 00:02:12 -0500

    simplified numeric constraint in routes.rb; plus minor fixes

--- hundis/config/routes.rb
+++ hundis/config/routes.rb
@@ -1,17 +1,24 @@
+#
+# def
+#
+
 Rails.application.routes.draw do
 
   id_cnstrt = /\d+/
 
-  get '/Home',          to: redirect('/')
+
+  root  'static_pages#Home'
+  get   '/Home',          to: redirect('/')
+
 
   # Authorization
   get   '/auth/login',    to: 'static_pages#login'
-  post  '/auth/login',    to: 'sessions#create'
+  post  '/auth/login',    to: 'sessions#create', as: 'login'
+  get   '/auth/logout',   to: 'sessions#destroy', as: 'logout'
   get   '/auth/register', to: 'static_pages#register'
   post  '/auth/register', to: 'users#create'
   get   '/auth/:provider/callback',  to: 'sessions#create'
   get   '/auth/failure',  to: redirect('/auth/login')
-  get   '/auth/logout',   to: 'sessions#destroy', as: 'logout'
 
   resources :sessions, only: [:create, :destroy]
 
@@ -23,24 +30,21 @@ Rails.application.routes.draw do
 
   # Problems Subsection
   get   '/problems/statistics',         to: 'problems#stats'
-  get   '/problems/:id',                to: 'problems#show', constraints: { id: id_cnstrt }
-  get   '/problems/:name/(*all)',       to: redirect('/temp') # TODO: reprocess names to IDs
-  get   '/problems/:id/submit/',        to: 'problems#showUpload',    constraints: { id: id_cnstrt }
-  post  '/problems/:id/submit/code',    to: 'problems#uploadCode',    constraints: { id: id_cnstrt }
-  post  '/problems/:id/submit/output',  to: 'problems#uploadOutput',  constraints: { id: id_cnstrt }
+  get   '/problems/:id',                to: 'problems#show',          id: id_cnstrt, as: :problem
+  get   '/problems/:id/submit/',        to: 'problems#showUpload',    id: id_cnstrt
+  post  '/problems/:id/submit/code',    to: 'problems#uploadCode',    id: id_cnstrt
+  post  '/problems/:id/submit/output',  to: 'problems#uploadOutput',  id: id_cnstrt
 
   # Users Subsection
-  get   '/users/current',             to: redirect('/temp') # TODO: reprocess names to IDs
-  get   '/users/:id',                 to: 'users#show',   constraints: { id: id_cnstrt }
-  get   '/users/:id/statistics',      to: 'users#stats',  constraints: { id: id_cnstrt }
-  get   '/users/:name/(*all)',        to: redirect('/temp') # TODO: reprocess names to IDs
+  get   '/users/:id',                 to: 'users#show',   id: id_cnstrt,  as: :user
+  get   '/users/:username/(*all)',    to: redirect('/temp') # TODO: reprocess names to IDs
+  get   '/users/:id/statistics',      to: 'users#stats',  id: id_cnstrt
 
   # Tourtnaments Subsection
-  get   '/tournaments/:id',             to: 'tournaments#show', constraints: { id: id_cnstrt }
-  get   '/tournaments/:id/statistics',  to: 'tournaments#stats', constraints: { id: id_cnstrt }
+  get   '/tournaments/:id',             to: 'tournaments#show',   id: id_cnstrt
+  get   '/tournaments/:id/statistics',  to: 'tournaments#stats',  id: id_cnstrt
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root  'static_pages#Home'
 
 end
