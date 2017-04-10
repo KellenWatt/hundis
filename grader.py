f2a7f3d6530b6677b8c6b6b66b6a2c0f0992f428
Jordan Giacone <Jmgiacone@gmail.com>
2017-04-10 15:59:09 -0500

    Implemented compilation and execution for one language

--- grader.py
+++ grader.py
@@ -1,3 +1,6 @@
+from subprocess import run, PIPE
+
+
 def main():
     # TODO: Parse cli arguments
     # TODO: Logging
@@ -5,7 +8,26 @@ def main():
     # TODO: Run code
     # TODO: Diff output
     # TODO: Floating-point validation
-    pass
+    output = cpp("test.txt")
+
+
+def cpp(input_file):
+    completed_process = \
+        run("g++ *.cpp -std=c++11 -o executable", stdout=PIPE, stderr=PIPE, encoding="UTF-8", shell=True)
+
+    if completed_process.returncode != 0:
+        exit(1)
+
+    completed_process = \
+        run("./executable < {} > program_output.txt".format(input_file), stdout=PIPE, stderr=PIPE, encoding="UTF-8",
+            shell=True)
+
+    if completed_process.returncode != 0:
+        exit(1)
+
+    return completed_process.stdout
+
+
 
 if __name__ == "__main__":
     main()
