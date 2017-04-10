390c9bb872b07906c75d0dc5fbe7caafa71e7244
Jordan Giacone <Jmgiacone@gmail.com>
2017-04-10 14:55:36 -0500

    Skeleton with a bunch of TODOs

---
+++ grader.py
@@ -0,0 +1,11 @@
+def main():
+    # TODO: Parse cli arguments
+    # TODO: Logging
+    # TODO: Compile code
+    # TODO: Run code
+    # TODO: Diff output
+    # TODO: Floating-point validation
+    pass
+
+if __name__ == "__main__":
+    main()
