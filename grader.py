5c98a788d1b3e89e7579093dc4de9f70748c66a1
Jordan Giacone <Jmgiacone@gmail.com>
2017-04-11 14:12:38 -0500

    Added output comparing functionality

--- grader.py
+++ grader.py
@@ -1,4 +1,5 @@
 from subprocess import run, PIPE
+import re
 
 
 def main():
@@ -8,9 +9,10 @@ def main():
     # TODO: Run code
     # TODO: Diff output
     # TODO: Floating-point validation
-    cpp("test.txt")
+    # cpp("test.txt")
 
-    compare_output()
+    # compare_output()
+    floating_point_validation()
 
 
 def cpp(input_file):
@@ -32,7 +34,51 @@ def compare_output():
     completed_process = run("diff test_output.txt program_output.txt", shell=True, stdout=PIPE, stderr=PIPE)
 
     if completed_process.returncode != 0:
-        exit(1)
+        return floating_point_validation()
+
+    return True
+
+
+def floating_point_validation(delta=.001):
+    sample_output = open("test_output.txt", "r")
+    program_output = open("program_output.txt", "r")
+
+    sample_floats = [word.strip("\n") for line in sample_output for word in line.split(" ")
+                     if re.match("(\+|-)?[0-9]+\.[0-9]+", word)]
+
+    program_floats = [word.strip("\n") for line in program_output for word in line.split(" ")
+                      if re.match("(\+|-)?[0-9]+\.[0-9]+", word)]
+
+    if len(sample_floats) == 0:
+        # No floats to look at
+        return False
+    elif len(sample_floats) != len(program_floats):
+        # Output is certainly wrong, there aren't the same number of floats
+        return False
+
+    # Same number of floats
+    for i in range(len(sample_floats)):
+        # Grab the correct answer
+        f = sample_floats[i]
+
+        # Convert it to a float
+        f = float(f)
+
+        # Create an upper and lower bound based on delta
+        window = f - delta, f + delta
+
+        # Grab the program answer
+        program_answer = program_floats[i]
+
+        # Convert it
+        program_answer = float(program_answer)
+
+        # Check if the program output is within that window
+        if not (window[0] <= program_answer <= window[1]):
+            return False
+
+    # Reached the end without returning false, return true
+    return True
 
 if __name__ == "__main__":
     main()
