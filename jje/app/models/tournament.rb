c1a94be6df80c70a132b190a3f04c2e5c0a58c1d
kthvg5 <kthvg5@mst.edu>
2017-04-11 00:38:54 -0500

    some stuff that will probably get overwritten in a second

--- jje/app/models/tournament.rb
+++ jje/app/models/tournament.rb
@@ -2,7 +2,7 @@ class Tournament < ApplicationRecord
   self.primary_key = 'tournament_id'
   has_many :languages, :class_name => 'TournamentLanguages', :foreign_key => [:tournament_id]
   has_many :contains, :class_name => 'Contains', :foreign_key => [:tournament_id]
-  has many :problems, through: :contains
+  has_many :problems, through: :contains
   has_many :competes_ins, :class_name => 'CompetesIns', :foreign_key => [:tournament_id]
   has_many :users, through: :competes_ins
 end
--- jje/config/routes.rb
+++ jje/config/routes.rb
@@ -4,6 +4,8 @@
 
 Rails.application.routes.draw do
 
+  get 'problems/index'
+
   id_cnstrt = /\d+/
 
 
