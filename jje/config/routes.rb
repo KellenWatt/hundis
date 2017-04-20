5eb719f02ce5e049f726555962940c598a0e15f0
Shawn McCormick <wsmkd6@mst.edu>
2017-04-19 21:21:00 -0500

    devise signup

--- jje/config/routes.rb
+++ jje/config/routes.rb
@@ -3,7 +3,12 @@
 #
 
 Rails.application.routes.draw do
-  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  devise_scope :user do
+    get "/sign_in" => "devise/sessions#new" # custom path to login/sign_in
+    get "/sign_up" => "devise/registrations#new", as: "new_user_registration" # custom path to sign_up/registration
+  end
+
+  devise_for :users, :controllers => {:registrations => "registrations", :omniauth_callbacks => "users/omniauth_callbacks" }
 
   id_cnstrt = /\d+/
 
