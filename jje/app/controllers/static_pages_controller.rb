edf438b94ab49bdc5a44ad44d3c007d326fe02c0
Samuel K. Grush <Samuel@Grush.org>
2017-04-23 17:29:13 -0500

    Implemented problems index page; smoothed out pagination for problems and users

--- jje/app/controllers/static_pages_controller.rb
+++ jje/app/controllers/static_pages_controller.rb
@@ -2,13 +2,24 @@ INDEX_PAGE_SIZE = 10
 
 class StaticPagesController < ApplicationController
   def users
-    @pg_count = (User.count / INDEX_PAGE_SIZE).ceil
+    @pg_count = (User.count / INDEX_PAGE_SIZE.to_f).ceil
     @pg = (params["pg"] || 1).to_i
 
     if @pg < 1 or @pg > @pg_count
-      raise "bad page number"
+      raise "bad page number; pg=#{@pg}"
     else
       @users = User.limit(INDEX_PAGE_SIZE).offset(INDEX_PAGE_SIZE * (@pg - 1))
+    end
+  end
+
+  def problems
+    @pg_count = (Problem.count / INDEX_PAGE_SIZE.to_f).ceil
+    @pg = (params["pg"] || 1).to_i
+
+    if @pg < 1 or @pg > @pg_count
+      raise "bad page number; pg=#{@pg}"
+    else
+      @problems = Problem.limit(INDEX_PAGE_SIZE).offset(INDEX_PAGE_SIZE * (@pg - 1))
     end
   end
 end
