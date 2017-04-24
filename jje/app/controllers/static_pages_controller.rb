a60db3a4d6a82aeeecfbaf707c1eb533375348d1
Samuel K. Grush <Samuel@Grush.org>
2017-04-23 19:29:32 -0500

    Implemented tournaments index page; modularized pagination; now in 'app/views/shared/_paginator'

--- jje/app/controllers/static_pages_controller.rb
+++ jje/app/controllers/static_pages_controller.rb
@@ -1,6 +1,7 @@
 INDEX_PAGE_SIZE = 10
 
 class StaticPagesController < ApplicationController
+
   def users
     @pg_count = (User.count / INDEX_PAGE_SIZE.to_f).ceil
     @pg = (params["pg"] || 1).to_i
@@ -20,6 +21,18 @@ class StaticPagesController < ApplicationController
       raise "bad page number; pg=#{@pg}"
     else
       @problems = Problem.limit(INDEX_PAGE_SIZE).offset(INDEX_PAGE_SIZE * (@pg - 1))
+    end
+  end
+
+  def tournaments
+    @now = DateTime.current()
+    @pg_count = (Tournament.count / INDEX_PAGE_SIZE.to_f).ceil
+    @pg = (params["pg"] || 1).to_i
+
+    if @pg < 1 or @pg > @pg_count
+      raise "bad page number; pg=#{@pg}"
+    else
+      @tourneys = Tournament.limit(INDEX_PAGE_SIZE).offset(INDEX_PAGE_SIZE * (@pg - 1))
     end
   end
 end
