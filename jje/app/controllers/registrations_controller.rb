4f01f372fa41f8bf586f817c4cb4a37b6f4067d6
Shawn McCormick <wsmkd6@mst.edu>
2017-04-25 14:28:03 -0500

    Registration

--- jje/app/controllers/registrations_controller.rb
+++ jje/app/controllers/registrations_controller.rb
@@ -35,7 +35,7 @@ class RegistrationsController < Devise::RegistrationsController
   protected
 
   def configure_permitted_parameters
-  	devise_parameter_sanitizer.permit(:signup, keys: [:display_name, :email, :password, :password_confirmation])
+  	devise_parameter_sanitizer.permit(:sign_up, keys: [:display_name, :email, :password, :password_confirmation])
   	devise_parameter_sanitizer.permit(:account_update, keys: [:display_name, :email, :password, :password_confirmation])
   end
 end
