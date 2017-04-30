b9cbe72775dfafdbb3af68f60ad426d9b581ae69
jniemeyer46 <jjniemeyer46@gmail.com>
2017-04-30 06:21:26 -0500

    Fixing everything I messed up I hope

--- jje/app/controllers/problems_controller.rb
+++ jje/app/controllers/problems_controller.rb
@@ -9,6 +9,55 @@ class ProblemsController < ApplicationController
   # POST /problems
   def create
     # TODO: handle problem creation
+    unless current_user.admin?
+      redirect_to :problems, flash: {error: 'Only administrators can create new problems.'}
+    end
+    keywords = params[:keywords][:allkeywords].split
+    tags = params[:tags][:alltags].split
+    # TODO: sanitize keywords/tags
+
+    @problem = Problem.new(problem_params)
+    if @problem.save
+      keywords.each do |kw|
+        @problem.keywords.create(keyword: kw)
+      end
+      tags.each do |tag|
+        @problem.tags.create(tag: tag)
+      end
+      redirect_to @problem, flash: {success: 'Problem created!'}
+    else
+      redirect_to :new_problem, flash: {error: "Failed to create problem: #{@problem.errors.full_messages}"}
+    end
+  end
+
+  # GET /problems/:id/edit
+  def edit
+    unless current_user.admin?
+      redirect_to :problems, flash: {error: 'Only administrators can edit problems.'}
+    end
+    @keywordstring = @problem.keywords.collect(&:keyword).join(' ')
+    @tagstring = @problem.tags.collect(&:tag).join(' ')
+  end
+
+  # PUT /problems/:id
+  def update
+    unless current_user.admin?
+      redirect_to :problems, flash: {error: 'Only administrators can create new problems.'}
+    end
+    keywords = params[:keywords][:allkeywords].split
+    tags = params[:tags][:alltags].split
+    # TODO: sanitize keywords/tags
+
+    if @problem.update(problem_params)
+      @problem.keywords.clear
+      keywords.each do |kw| @problem.keywords.create(keyword: kw) end
+      @problem.tags.clear
+      tags.each do |tag| @problem.tags.create(tag: tag) end
+
+      redirect_to @problem, flash: {success: 'Problem updated!'}
+    else
+      redirect_to @problem, flash: {error: "Failed to update problem: #{@problem.errors.full_messages}"}
+    end
   end
 
   # GET /problems/statistics
@@ -18,8 +67,8 @@ class ProblemsController < ApplicationController
 
   # GET /problems/:id
   def show
-    @keywords = ProblemKeyword.where(problem_id: params[:id])
-    @tags = ProblemTag.where(problem_id: params[:id])
+    #@keywords = ProblemKeyword.where(problem_id: params[:id])
+    #@tags = ProblemTag.where(problem_id: params[:id])
   end
 
   # GET /problems/:id/submit
@@ -60,5 +109,9 @@ class ProblemsController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_problem
       @problem = Problem.find(params[:id])
+    end
+
+    def problem_params
+      params.require(:problem).permit(:name, :score, :description)
     end
 end
--- jje/app/models/problem.rb
+++ jje/app/models/problem.rb
@@ -1,7 +1,7 @@
 class Problem < ApplicationRecord
   self.primary_key = 'problem_id'
-  has_many :keywords, :class_name => 'ProblemKeyword', :foreign_key => [:problem_id]
-  has_many :tags, :class_name => 'ProblemTag', :foreign_key => [:problem_id]
+  has_many :keywords, :class_name => 'ProblemKeyword', dependent: :destroy
+  has_many :tags, :class_name => 'ProblemTag', dependent: :destroy
   
   def to_s
     "Name: #{self.name}, ID: #{self.problem_id}"
