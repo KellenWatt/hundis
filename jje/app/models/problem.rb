3cc042ad48093fe4b04f165cbe2e3044eeca2cb3
Samuel K. Grush <Samuel@Grush.org>
2017-04-30 15:01:10 -0500

    Model cleanup and homepage rework

--- jje/app/models/problem.rb
+++ jje/app/models/problem.rb
@@ -2,6 +2,7 @@ class Problem < ApplicationRecord
   self.primary_key = 'problem_id'
   has_many :keywords, :class_name => 'ProblemKeyword', dependent: :destroy
   has_many :tags, :class_name => 'ProblemTag', dependent: :destroy
+  has_many :submissions, class_name: 'UserSubmission', dependent: :destroy
   
   def to_s
     "Name: #{self.name}, ID: #{self.problem_id}"
