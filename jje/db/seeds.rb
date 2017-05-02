fbcb693265252bc0e3221b61e0e6c8d7070e3cd7
jniemeyer46 <jjniemeyer46@gmail.com>
2017-05-01 21:03:11 -0500

    Problems and tournament pages look better, onto user.

--- jje/db/seeds.rb
+++ jje/db/seeds.rb
@@ -36,6 +36,30 @@ Problem.create!(name: 'Limbo', score: 5, description: 'stuff and things like tha
 Problem.create!(name: 'Chaos', score: 8, description: 'stuff and things like that for all people', path: 'some path')
 Problem.create!(name: 'Domination', score: 10, description: 'stuff and things like that for all people on', path: 'some path')
 Problem.create!(name: 'Entertainer', score: 50, description: 'stuff and things like that for all people on Earth', path: 'some path')
+Problem.create!(name: 'The Doctor is in', score: 50, description: 'For this assignment, you need to submit a file called ‘mystringmap.h’ and any other necessary implementation files. Remember to put your name and section at the top of all your files.
+Problem:
+Dr. Zoidberg is a great doctor, but he sometimes forget some of his training. In the 30th century, a doctor has to master hundreds of different anatomies and physiologies. Dr. Zoidberg has mastered plenty of them, but he sometimes has a problem distinguishing between different lifeforms. Who can remember how many livers a Human, a Martian, and a Decapodian each has? In order to help him remember who is what, Dr. Zoidberg has started to keep notes on all his patients about which species they belong to, and has decided that it is finally time to modernize his process. He has requested you to provide him with decision software to quickly and easily determine a patient’s kind.
+
+Medicine is serious business.
+
+Your job is to write a program that uses a Hash Table to implement a "StringMap": a set of <key,value> pairs where the key is a string. 
+Testing:
+Use the provided tester file to check if your implementation is working correctly. 
+The program ‘maptester.cpp’ uses the ‘MyStringMap’ class and the intended output is ‘mapoutput.txt’.
+
+Notice:
+You are expected to derive the ‘AbstractStringMap’ class.
+The function ‘valueOf()’ is expected to “throw” errors. 
+You are required to use a Hash-Table.
+Your ‘print()’ function may output the <key,value> pairs is a different order than the one shown in the output file. (depending on your Hash-Table design and Hash function). This is normal and expected.
+Useful Hints:
+Carefully read the comments of each member function. 
+Write down an algorithm for the function before you start coding it. 
+Develop your member functions one at a time, starting from the simplest ones!
+Move to the next function only after the previous one has been tested. 
+Trying to code the whole class and then remove the bugs may prove to be too big a task.
+Use the test functions one at a time.
+', path: 'some path')
 
 ProblemKeyword.create!(problem_id: 1, keyword: "cat")
 ProblemKeyword.create!(problem_id: 2, keyword: "dog")
