0e4e39c1f91ee7b6e55150b44869582a81c9a41f
Shawn McCormick <wsmkd6@mst.edu>
2017-05-01 20:20:21 -0500

    Show solved problems

--- jje/app/controllers/users_controller.rb
+++ jje/app/controllers/users_controller.rb
@@ -8,7 +8,8 @@ class UsersController < ApplicationController
   # GET /users/1.json
   def show
     @user = User.find(params[:id])
-    @tournaments = Tournament.joins(:competes_ins).where(:user_id == @user.user_id)
+    @tournaments = Tournament.joins(:competes_ins).where(["user_id = %i", @user.user_id])
+    @solves = Problem.joins(:submissions).where(["user_id = %i and solved", @user.user_id])
   end
 
   # POST /users
